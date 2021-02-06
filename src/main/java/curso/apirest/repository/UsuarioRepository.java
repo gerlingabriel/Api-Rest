05471792f6a84489be1d7ed8631af83550c69109
Gabriel Gerlin <75265629+gerlingabriel@users.noreply.github.com>
2021-02-06 15:54:21 -0300

    Update UsuarioRepository.java

--- src/main/java/curso/apirest/repository/UsuarioRepository.java
+++ src/main/java/curso/apirest/repository/UsuarioRepository.java
@@ -2,6 +2,10 @@ package curso.apirest.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -32,6 +36,21 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	@Modifying
 	@Query(nativeQuery = true, value = "INSERT INTO usuarios_role (usuario_id, role_id) VALUES (?1, (SELECT id FROM role where cargo = 'ROLE_USER'));")
 	void acessoPadrao(Long id);
+
+
+	default Page<Usuario> findUserByNamePage(String nome, PageRequest pageRequest) {
+		
+		Usuario usuario = new Usuario();
+		usuario.setNome(nome);
+		
+		ExampleMatcher exampleMatcher = ExampleMatcher.matchingAny()
+				.withMatcher("nome", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase());
+		
+		Example<Usuario> example = Example.of(usuario, exampleMatcher);
+		
+		return findAll(example, pageRequest);
+	}
+
 
 
 }
